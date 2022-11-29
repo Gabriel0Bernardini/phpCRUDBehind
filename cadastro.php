18c9759f30620b6c1008ed3c1b9e219c2bd9ad32
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-11-29 11:36:54 -0300

    commit

--- cadastro.php
+++ cadastro.php
@@ -18,22 +18,22 @@
             <p class="p1" >Cadastro</p>
             <form class="form1" action="insert.php" method="post">
                 <label for="txtNome" >Primeiro nome:</label><br>
-                <input type="text" id="txtNome" name="txtNome" class="input1" required placeholder ="Digite o nome" style="margin-top: 3px;font-family:'DogicaBold';" required><br>
+                <input type="text" id="txtNome" name="txtNome" class="input1" required placeholder ="Digite o nome" style="" required><br>
                 
                 <label for="txtSobrenome" >Sobrenome:</label><br>
-                <input type="text" id="txtSobrenome" name="txtSobrenome" class="input1" required placeholder ="Digite o sobrenome" style="font-family:'DogicaBold';" required><br>
+                <input type="text" id="txtSobrenome" name="txtSobrenome" class="input1" required placeholder ="Digite o sobrenome" style="" required><br>
 
                 <label for="txtNick">Nickname:</label><br>
-                <input type="text" id="txtNick" name="txtNick" class="input1" required placeholder ="Digite o nick" style="font-family:'DogicaBold';" required><br>
+                <input type="text" id="txtNick" name="txtNick" class="input1" required placeholder ="Digite o nick" style="" required><br>
 
                 <label for="txtEmail">E-mail:</label><br>
-                <input type="text" id="txtEmail" name="txtEmail" class="input1" required placeholder ="Digite o e-mail" style="font-family:'DogicaBold';"><br>
+                <input type="text" id="txtEmail" name="txtEmail" class="input1" required placeholder ="Digite o e-mail" style=""><br>
 
                 <label for="txtSenha">Senha:</label><br>
-                <input type="password" id="txtSenha" name="txtSenha" class="input1" required placeholder ="Digite a senha" style="font-family:'DogicaBold';"><br>
+                <input type="password" id="txtSenha" name="txtSenha" class="input1" required placeholder ="Digite a senha" style=""><br>
 
                 <label for="ConfirmSenha"> Confirme a senha:</label><br>
-                <input type="password" id="ConfirmSenha" name="ConfirmSenha" class="input1" required placeholder ="Confirme a senha" style="font-family:'DogicaBold';"><br>
+                <input type="password" id="ConfirmSenha" name="ConfirmSenha" class="input1" required placeholder ="Confirme a senha" style=""><br>
 
                 <label for="TipoUsuario"> Selecione o tipo de usuário:</label><br>
                 <input type="radio"  name="TipoUsuario" value="ADMIN" class="input1" style="font-family:'DogicaBold';">ADMIN

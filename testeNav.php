1e2b4591b46ad21fcb9e7ecb8ea037fb521a649d
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-01 10:33:15 -0300

    correcaoTesteNav

--- testeNav.php
+++ testeNav.php
@@ -40,40 +40,5 @@
   </div> 
 </div>
 </nav>
-
-
-<div class = "container1">
-        <div class="div1">
-            <p class="p1" >Cadastro</p>
-            <form class="form1" action="insert.php" method="post">
-                <label for="txtNome" >Primeiro nome:</label><br>
-                <input type="text" id="txtNome" name="txtNome" class="input1"  placeholder ="Digite o nome" required><br>
-                
-                <label for="txtSobrenome" >Sobrenome:</label><br>
-                <input type="text" id="txtSobrenome" name="txtSobrenome" class="input1"  placeholder ="Digite o sobrenome" required><br>
-
-                <label for="txtNick">Nickname:</label><br>
-                <input type="text" id="txtNick" name="txtNick" class="input1"  placeholder ="Digite o nick" required ><br>
-
-                <label for="txtEmail">E-mail:</label><br>
-                <input type="text" id="txtEmail" name="txtEmail" class="input1"  placeholder ="Digite o e-mail" required><br>
-
-                <label for="txtSenha">Senha:</label><br>
-                <input type="password" id="txtSenha" name="txtSenha" class="input1"  placeholder ="Digite a senha" required><br>
-
-                <label for="ConfirmSenha"> Confirme a senha:</label><br>
-                <input type="password" id="ConfirmSenha" name="ConfirmSenha" class="input1"  placeholder ="Confirme a senha" required><br>
-
-                <label for="TipoUsuario"> Selecione o tipo de usuário:</label><br>
-                <input type="radio"  name="TipoUsuario" value="ADMIN" class="input1" style="font-family:'DogicaBold';">ADMIN
-                <input type="radio" name="TipoUsuario" value="COMUM" class="input1" style="font-family:'DogicaBold';">COMUM
-
-                <input type="submit" value="Cadastrar" nome="btnCadastrar" class="BotaoCadastrar">
-                <input type="reset" value="Cancelar" nome="btnCancelar" class="BotaoCadastrar">
-            </form>
-
-            <p class="p2">Se você já tem um cadastro <a href="login.php" style="color: rgb(4, 116, 4);;">clique aqui para fazer login</a></p>
-        </div>
-    </div>
 </body>
 </html>

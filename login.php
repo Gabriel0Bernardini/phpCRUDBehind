942b2aa72c7904f9ade9b07f3ed04eea15801b91
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-01 10:50:20 -0300

    Inplementação de Nav 5

--- login.php
+++ login.php
@@ -5,13 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Behind The Bushes</title>
-    <link rel="stylesheet" href="loginStyle.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="LoginStyle.css">
 </head>
 <body>
-    <p style="margin-left:-86%">
-    <a href="index.php" style="background-color: black; ">
-    <img src="Images/WoodenSign.png" style="height: 100px;">
-    </a></p>
+
+    <nav class="navbar"  style="background: rgb(4, 64, 3);">
+        <div class="container-fluid" style="justify-content:right ;">
+            <h1 class="tituloNav">Behind The Bushes</h1>
+            <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="p-4 link" style="display: block; ">
+                <a href="index.php" class="link">Home</a> 
+                <a href="cadastro.php" class="link">Cadastro</a>
+                <a href="login.php" class="link">Login</a>
+                <a href="selectUsuario.php" class="link">Usuarios Cadastrados</a>
+            </div> 
+        </div>
+    </nav>
 
     <div class = "container1">
         <div class="div1">

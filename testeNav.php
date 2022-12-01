e16c9d95226424837d6abb63431b74f7f804ad1d
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-01 10:43:46 -0300

    Implementação de Nav 3

--- testeNav.php
+++ testeNav.php
@@ -9,12 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link href="Indexstyle.css" rel="stylesheet">
-    <link href="editarStyle.css" rel="stylesheet">
-    <link href="cadastroStyle.css" rel="stylesheet">
 </head>
 <body>
 <style>
+body{
+  text-align: center;
+  background-image: url(Images/Bush1.png);
+  background-repeat: repeat;
+}
   .link{
     text-decoration: none;
     color: white;
@@ -22,11 +24,33 @@
     font-size: 22px;
     margin-left: 5px;
   }
+
+  .tituloNav{
+    font-family: 'Daydream', Times, serif;
+    color:white;
+    margin-right:27%;
+    margin-left:23%;
+    margin-bottom:1%;
+    padding-top: 1%;
+}
+@font-face {
+    font-family: 'Daydream';
+    src: url('Fonts/Daydream.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face{
+    font-family: 'DogicaBold';
+    src:url('Fonts/TTF/dogicabold.ttf');
+    font-weight: normal;
+    font-size: normal;
+}
 </style>
 
 <nav class="navbar"  style="background: rgb(4, 64, 3);">
   <div class="container-fluid" style="justify-content:right ;">
-    <h1 style="color:white;margin-right:27%;margin-left:23%;margin-bottom:1%;padding-top: 1%;">Behind The Bushes</h1>
+    <h1 class="tituloNav">Behind The Bushes</h1>
     <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
       <span class="navbar-toggler-icon"></span>
     </button>

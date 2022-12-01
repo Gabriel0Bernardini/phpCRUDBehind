ff96a1661c155ea27d8a79d9bad6d8a624974eff
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-01 10:31:49 -0300

    Implementação da Nav2

--- cadastro.php
+++ cadastro.php
@@ -9,40 +9,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="cadastroStyle.css">
-    <link href="Indexstyle.css" rel="stylesheet">
-    <link href="editarStyle.css" rel="stylesheet">
+    <link rel="stylesheet" href="CadastroStyle.css">
 </head>
 <body>   
-    <style>
-  .link{
-    text-decoration: none;
-    color: white;
-    font-family: 'DogicaBold';
-    font-size: 22px;
-    margin-left: 5px;
-  }
-</style>
 
-<nav class="navbar"  style="background: rgb(4, 64, 3);">
-  <div class="container-fluid" style="justify-content:right ;">
-    <h1 style="color:white;margin-right:27%;margin-left:23%;margin-bottom:1%;padding-top: 1%;">Behind The Bushes</h1>
-    <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
-<div class="collapse" id="navbarToggleExternalContent">
-<div class="p-4 link" style="display: block; ">
-    <a href="index.php" class="link">Home</a> 
-    <a href="cadastro.php" class="link">Cadastro</a>
-    <a href="login.php" class="link">Login</a>
-    <a href="selectUsuario.php" class="link">Usuarios Cadastrados</a>
-  </div> 
-</div>
-</nav>
+    <nav class="navbar"  style="background: rgb(4, 64, 3);">
+        <div class="container-fluid" style="justify-content:right ;">
+            <h1 class="tituloNav">Behind The Bushes</h1>
+            <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="p-4 link" style="display: block; ">
+                <a href="index.php" class="link">Home</a> 
+                <a href="cadastro.php" class="link">Cadastro</a>
+                <a href="login.php" class="link">Login</a>
+                <a href="selectUsuario.php" class="link">Usuarios Cadastrados</a>
+            </div> 
+        </div>
+    </nav>
 
 
-<div class = "container1">
+    <div class = "container1">
         <div class="div1">
             <p class="p1" >Cadastro</p>
             <form class="form1" action="insert.php" method="post">

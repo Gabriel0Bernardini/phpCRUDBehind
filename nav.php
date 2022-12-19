5aeb17b59073cc1136f22b233fa086b7deb62a23
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-19 17:46:22 -0300

    ImplementaçãoCarrossel_Novo Index

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="navTituloStyle.css">
 <nav class="navbar"  style="background: rgb(4, 64, 3);">
   <div class="container-fluid" style="justify-content:right ;">
-    <h1 class="tituloNav">Behind The Bushes</h1>
+    <h1  class="tituloNav">Behind The Bushes</h1>
     
     <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
       <span class="navbar-toggler-icon"></span>
@@ -9,10 +9,10 @@
   </div>
 <div class="collapse" id="navbarToggleExternalContent">
 <div class="p-4 link" style="display: block; ">
-    <a href="index.php" class="link">Home</a> 
-    <a href="cadastro.php" class="link">Cadastro</a>
-    <a href="login.php" class="link">Login</a>
-    <a href="selectUsuario.php" class="link">Usuários</a>
+    <a href="index.php" class="link aNavLink">Home</a> 
+    <a href="cadastro.php" class="link aNavLink">Cadastro</a>
+    <a href="login.php" class="link aNavLink">Login</a>
+    <a href="selectUsuario.php" class="link aNavLink">Usuários</a>
   </div> 
 </div>
 </nav>
--- novoIndex.php
+++ novoIndex.php
@@ -15,6 +15,25 @@
 <?php
     require_once("nav.php");
 ?>
+<br>    
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/Slide1Redimencionado2.jpg" class="d-block w-100 active" alt="Slide do Jogo 1">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Slide2Redimencionado2.jpg" class="d-block w-100" alt="Slide do Jogo 2">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 </body>
 </html>

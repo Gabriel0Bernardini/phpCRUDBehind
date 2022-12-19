fe01dbb3ba530ed737b72f6119cb76cfa0f41ce6
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2022-12-19 20:07:22 -0300

    NovoTitulo1

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="navTituloStyle.css">
 <nav class="navbar"  style="background: rgb(4, 64, 3);">
   <div class="container-fluid" style="justify-content:right ;">
-    <h1  class="tituloNav">Behind The Bushes</h1>
+    <h1  class="containerTituloNav"><img src="Images/BehindTheBushesTitle2.png" class="tituloNav"></h1>
     
     <button class="navbar-toggler" style="background-image: url(Images/woodenTexture.jpg);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
       <span class="navbar-toggler-icon"></span>

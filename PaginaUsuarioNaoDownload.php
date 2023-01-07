b12152d918b96fd4b3f116efc97c5c0a14d06efa
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2023-01-07 13:46:38 -0300

    Download Funcionando Com Login

---
+++ PaginaUsuarioNaoDownload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>USUARIO NÃO LOGADO!</title>
+    <link rel="stylesheet" href="PaginaUsuarioNaoLogado.css">
+</head>
+<body>
+    <div class = "container1">
+        <div class="div1">
+            <p class="p1">Usuario não conectado!</p>
+            <p class="p2">Para fazer o download do jogo por favor<br><a href="login.php" style="color: rgb(4, 116, 4);;">clique aqui para logar</a></p>
+            <p class="p2">Se é sua primeira vez,<br><a href="login.php" style="color: rgb(4, 116, 4);;">clique aqui para se cadastrar</a> </p>
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NovoIndex</title>
+    <title>Home</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
@@ -77,6 +77,10 @@
     </div>
   </div>
 
- <button type="button" onclick="location.href='download.php?file=BehindTheBushes_20221206_v11.rar'"class="botaoDownload">DOWNLOAD</button> 
+ <button type="button" class="botaoDownload" <?php  
+    if(isset($_SESSION["email"]) ){?>
+      onclick="location.href='download.php?file=BehindTheBushes_20221206_v11.rar'"
+    <?php }else{?> onclick="location.href='PaginaUsuarioNaoDownload.php'" <?php }?> 
+    >DOWNLOAD</button> 
 </body>
 </html>

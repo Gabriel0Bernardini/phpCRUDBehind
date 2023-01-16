994eab46cddc6240bf20e2a080920014f00d8436
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2023-01-16 13:48:28 -0300

    Mudança de index

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-  require_once("conexao.php");
-  session_start();
+require_once("conexao.php");
+session_start();
 
-  
+
 ?>
 
 <!DOCTYPE html>
@@ -12,64 +12,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="indexStyle.css" rel="stylesheet">
+     <!-- CSS only -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link href="indexstyle.css" rel="Stylesheet">
+    
 </head>
 <body>
-  <div>
-  <?php
-      include("header.php");
-  ?>
-  </div>
-  <br><br><br><br><br>    
-  <div class="container-fluid">
-  <div id="carouselExampleFade" class=" carousel slide carousel-fade " data-bs-ride="carousel" style="width: 46%;margin-left:2%;margin-top: 9%;
-      z-index: 0;
-      border-radius: 50px; 
-      border-style: solid;
-      border-width: 5px; 
-      border-color: rgb(1, 19, 5);
-      box-shadow: 5px 10px 30px 10px rgb(0, 17, 4), 5px 10px 30px 10px rgb(2, 63, 16);
-      display: flex;
-      justify-content: center;">
+<?php
+    include("header.php");
+?>
+<div class="backgroundCarrossel">
+<div id="carouselExampleIndicators" class="carousel slide carousel-fade child" style="
+    z-index:0;
+    width: 50%;
+    margin-top: 9%; 
+    border-style: none;
+    display: block;
+    justify-content: center;">
 
-    <div class="carousel-inner" style="border-radius: 45px;">
-      <div class="carousel-item active">
-        <img src="Images/Slide1.jpeg" class="d-block w-100 active" alt="Slide do Jogo 1" >
-      </div>  
-      <div class="carousel-item">
-        <img src="Images/Slide2.jpeg" class="d-block w-100" alt="Slide do Jogo 2">
-      </div>
-      <div class="carousel-item">
-        <img src="Images/Slide3.jpeg" class="d-block w-100" alt="Slide do Jogo 3">
-      </div>
-      <div class="carousel-item">
-        <img src="Images/Slide4.jpeg" class="d-block w-100" alt="Slide do Jogo 4">
-      </div>
-      <div class="carousel-item">
-        <img src="Images/Slide5.jpeg" class="d-block w-100" alt="Slide do Jogo 5">
-      </div>
-      <div class="carousel-item">
-        <img src="Images/Slide6.jpeg" class="d-block w-100" alt="Slide do Jogo 6">
-      </div>
-      <div class="carousel-item">
-        <img src="Images/Slide7.jpeg" class="d-block w-100" alt="Slide do Jogo 7">
-      </div>
+ 
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/Slide1.jpeg" class="d-block w-100" alt="...">
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item">
+      <img src="Images/Slide2.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Slide3.jpeg" class="d-block w-100" alt="...">
+    </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
 
-  
+<button type="button" class="botaoDownload" <?php  
+    if(isset($_SESSION["email"]) ){?>
+      onclick="location.href='./DownloadFiles/BehindTheBushes_20221206_v11.zip'"
+    <?php }else{?> onclick="location.href='PaginaUsuarioNaoDownload.php'" <?php }?> 
+    >DOWNLOAD</button> 
+
+
     <div class="div1" style="margin-right: 2%;">
       <p class="p1">
         Behind the Bushes é um TopDown Shooter onde o personagem principal, Jerson, se encontra <br>perdido em uma floresta densa, rodeado de mata.<br>
@@ -79,11 +72,5 @@
       </p>
     </div>
   </div>
-
- <button type="button" class="botaoDownload" <?php  
-    if(isset($_SESSION["email"]) ){?>
-      onclick="location.href='./DownloadFiles/BehindTheBushes_20221206_v11.zip'"
-    <?php }else{?> onclick="location.href='PaginaUsuarioNaoDownload.php'" <?php }?> 
-    >DOWNLOAD</button> 
 </body>
 </html>

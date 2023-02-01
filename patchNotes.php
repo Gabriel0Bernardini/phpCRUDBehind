22c426e96a8667286275658c4a854680ce89da22
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2023-02-01 17:07:11 -0300

    Patch notes 6(Botoes delete e edit)

--- patchNotes.php
+++ patchNotes.php
@@ -15,7 +15,7 @@ session_start();
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/font-awesome.min.css">
     <title>Patch Notes</title>
 </head>
 <body>
@@ -23,16 +23,7 @@ session_start();
     include("Header.php");
 ?>
 <br><br><br><br><br>
-<?php
-    if((isset($_SESSION["email"])) && ($_SESSION["tipo"]==="ADMIN")){
-        ?>
-        <div class="div1">
-        <a href="PatchCadastrar.php" style="text-decoration:none"><p class="p1">Cadastrar Patch</p></a>
-    <?php
-        }
-    ?>
-        </div>
-<br><br><br><br><br>
+
 
 <?php
 
@@ -44,7 +35,15 @@ session_start();
         ?>
     <div class="container1">
         <div class="div2">
-            <h1 class="p2">Patch Notes</h1>
+            <h1 class="p2">Patch Notes 
+            <?php
+            if((isset($_SESSION["email"])) && ($_SESSION["tipo"]==="ADMIN")){
+        ?>     <a href="PatchCadastrar.php" style="text-decoration:none"><i class="fa-sharp fa-solid fa-circle-plus" style="color: black;"></i></a>
+         <?php
+        }
+        ?>
+        </h1>
+            
     <?php
     while($exibir = $dadosPatch->fetch_assoc()){
     ?>
@@ -52,6 +51,23 @@ session_start();
     <dl class="dl1">
         <dt class="dt1" > <?php echo $exibir["NomeJogo"]?> versão <?php echo $exibir["VersaoPatch"]?><p class="pData"><?php echo $exibir["DataPatch"]?></p></dt>
         <dd class="dd1"><?php echo $exibir["DescricaoPatch"]?></dd>
+        <?php
+            if((isset($_SESSION["email"])) && ($_SESSION["tipo"]==="ADMIN")){
+        ?>   
+        <dd class="dd2">
+            <a href="#">
+                <button type="button" class="btn btn-danger">
+                    <i class="fa-solid fa-trash-can" style="color:white"></i> Deletar Patch
+                </button>
+            </a>
+            <a href="#">
+            <button type="button" class="btn btn-primary">
+                <i class="fa-solid fa-pen-to-square" style="color: white;"></i> Editar Patch
+            </button>
+            </a>
+        <?php
+        }
+        ?>
     </dl>
     </div>
     <?php
